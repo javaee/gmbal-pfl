275f6f420b1434b5d4c48bf9f5be1a4214fcf2dd
Ken Cavanaugh <Ken.Cavanaugh@sun.com>
2009-11-26 01:09:22 -0800

    Initial creation

---
+++ src/org/glassfish/pfl/App.java
@@ -0,0 +1,13 @@
+package org.glassfish.pfl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
