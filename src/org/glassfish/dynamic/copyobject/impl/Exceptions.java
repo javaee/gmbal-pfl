6e4ba057c4ed5235602756d4f2c1a1530047a705
Ken Cavanaugh <Ken.Cavanaugh@oracle.com>
2010-11-14 17:52:41 -0800

    - Completed basic structure of libraries

---
+++ src/org/glassfish/dynamic/copyobject/impl/Exceptions.java
@@ -0,0 +1,81 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * 
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ * 
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.glassfish.dynamic.copyobject.impl;
+
+import org.glassfish.logex.Chain;
+import org.glassfish.logex.ExceptionWrapper;
+import org.glassfish.logex.Log;
+import org.glassfish.logex.LogLevel;
+import org.glassfish.logex.Message;
+import org.glassfish.logex.WrapperGenerator;
+
+import org.glassfish.dynamic.copyobject.spi.ReflectiveCopyException ;
+
+/** Exception wrapper class.  The logex WrapperGenerator uses this interface
+ * to generate an implementation which returns the appropriate exception, and
+ * generates a log report when the method is called.  This is used for all
+ * implementation classes in this package.
+ *
+ * The exception IDs are allocated in blocks of EXCEPTIONS_PER_CLASS, which is
+ * a lot more than is needed, but we have 32 bits for IDs, and multiples of
+ * a suitably chosen EXCEPTIONS_PER_CLASS (like 100 here) are easy to read in
+ * error messages.
+ *
+ * @author ken
+ */
+@ExceptionWrapper( idPrefix="ORBOCOPY" )
+public interface Exceptions {
+    static final Exceptions self = WrapperGenerator.makeWrapper(
+        Exceptions.class) ;
+
+    // Allow 100 exceptions per class
+    static final int EXCEPTIONS_PER_CLASS = 100 ;
+
+// FallbackCopierImpl
+    static final int FB_START = 1 ;
+
+    @Message( "Object copy failed on copy of {0} which has type {1}" )
+    @Log( id = FB_START + 0, level=LogLevel.FINE )
+    void failureInFallback(
+        @Chain ReflectiveCopyException exc, Object obj, Class cls );
+
+// XXX possibly add more logging elsewhere in the object copier
+}
