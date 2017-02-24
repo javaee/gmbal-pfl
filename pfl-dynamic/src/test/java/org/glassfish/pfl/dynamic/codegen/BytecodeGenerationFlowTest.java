7c9e3a7dacaeaa933bfe9b712ec406018dbb6cab
Russell Gold <russell.gold@oracle.com>
2017-02-24 15:03:36 -0500

    move codegen tests to pfl-dynamic module

---
+++ pfl-dynamic/src/test/java/org/glassfish/pfl/dynamic/codegen/BytecodeGenerationFlowTest.java
@@ -0,0 +1,11 @@
+package org.glassfish.pfl.dynamic.codegen;
+
+/**
+ * The main test suite for code generation.  This tests all of the different patterns of code generation.
+ */
+public class BytecodeGenerationFlowTest extends FlowTestSuiteBase {
+
+    public BytecodeGenerationFlowTest() {
+        super(true, false);
+    }
+}
---
+++ pfl-dynamic/src/test/java/org/glassfish/pfl/dynamic/codegen/CodegenTestSuite.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.glassfish.pfl.dynamic.codegen;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ *
+ * @author ken
+ */
+public class CodegenTestSuite extends TestCase {
+    
+    public CodegenTestSuite(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("CodegenTestSuite");
+        return suite;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
